95b512d1a3aff16329713c1105185a894f36338f
Ömer Faruk Kaya <omerf.kaya97@gmail.com>
2023-09-09 17:07:17 +0300

    Implementation of getShipmentSize method

--- src/main/java/com/trendyol/shipment/Basket.java
+++ src/main/java/com/trendyol/shipment/Basket.java
@@ -1,13 +1,36 @@
 package com.trendyol.shipment;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class Basket {
 
     private List<Product> products;
+	private enum Constant {
+		SHIPMENT_SIZE_THRESHOLD(3);
 
+		public final int value;
+
+		Constant(int value){
+			this.value = value;
+		}
+	}
     public ShipmentSize getShipmentSize() {
-        return null;
+    	HashMap<ShipmentSize, Integer> shipmentSizeAmounts = new HashMap<>();
+		ShipmentSize largestProductSize = null;
+
+		for (Product product : products) {
+    		ShipmentSize productSize = product.getSize();
+    		int productSizeShipmentAmount = shipmentSizeAmounts.getOrDefault(productSize, 0) + 1;
+    		shipmentSizeAmounts.put(productSize, productSizeShipmentAmount);
+    		if(productSizeShipmentAmount == Constant.SHIPMENT_SIZE_THRESHOLD.value) {
+    			return productSize.returnNextBiggerSize();
+    		}
+			if(largestProductSize == null || productSize.isBigger(largestProductSize)) {
+    	    	largestProductSize = productSize;
+    	    }
+    	}
+        return largestProductSize;
     }
 
     public List<Product> getProducts() {
--- src/main/java/com/trendyol/shipment/ShipmentSize.java
+++ src/main/java/com/trendyol/shipment/ShipmentSize.java
@@ -1,9 +1,23 @@
 package com.trendyol.shipment;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum ShipmentSize {
 
     SMALL,
     MEDIUM,
     LARGE,
-    X_LARGE
+    X_LARGE;
+    
+    public ShipmentSize returnNextBiggerSize() {
+        List<ShipmentSize> list = Arrays.stream(ShipmentSize.values()).toList();
+        ShipmentSize biggestSize = list.get(list.size() - 1);
+        return list.stream().skip(list.indexOf(this) + 1).findFirst().orElse(biggestSize);
+    }
+    
+    public boolean isBigger(ShipmentSize shipmentSize){
+        return this.compareTo(shipmentSize) > 0;
+    }
 }
+
